808c619e4cfddea5dc63235be2fb13c1a569d560
yanhackcode15 <iamyanhu@gmail.com>
2025-04-10 11:55:39 -0700

    Done

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 let humanScore = 0;
 let computerScore = 0;
-// console.log(getComputerChoice())
-// console.log(getHumanChoice())
 
 function getComputerChoice() {
     //randomly return one of the three choices
@@ -17,60 +15,96 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice(){
-    return prompt("Your turn!")
-}
+// function getHumanChoice(){
+//     return prompt("Your turn!")
+// }
 
 function playRound(humanChoice, computerChoice){
-
+    const result = document.getElementById('result');
+    const score = document.getElementById('score');   
     if (humanChoice.toLowerCase()=='paper'){
         switch(computerChoice.toLowerCase()){
-            case 'paper':
-                console.log("You tie! Paper can't beat paper.");
+            case 'paper'://tie
+                result.textContent = "You tie! Paper can't beat paper.";
+                score.textContent = `Computer: ${computerScore} | Human: ${humanScore}`
                 break;
-            case 'rock':
-                console.log("You win! Paper beats Rock.");
+            case 'rock'://human wins
+                humanScore++;
+                result.textContent = "You win! Paper beats Rock.";
+                score.textContent = `Computer: ${computerScore} | Human: ${humanScore}`
+
                 break;
-            case 'scissors':
-                console.log("You lose! Scissors cut Paper.");
+            case 'scissors'://computer wins
+                computerScore++;
+                result.textContent = "You lose! Scissors cut Paper.";
+                score.textContent = `Computer: ${computerScore} | Human: ${humanScore}`
+
+                // console.log("You lose! Scissors cut Paper.");
                 break;
             default:
-                console.log("What happened?")
+                result.textContent = "OOPS"
+                score.textContent = `Computer: ${computerScore} | Human: ${humanScore}`
+
+                // console.log("What happened?")
         }
     } else if (humanChoice.toLowerCase()=='rock'){
         switch(computerChoice.toLowerCase()){
-            case 'paper':
-                console.log("You lose! Paper beats rock.");
+            case 'paper'://human loses
+                computerScore++;
+                result.textContent = "You lose! Paper beats rock.";
+                score.textContent = `Computer: ${computerScore} | Human: ${humanScore}`
+
+                // console.log("You lose! Paper beats rock.");
                 break;
-            case 'rock':
-                console.log("You tie! Rock on Rock.");
+            case 'rock': //ties
+                result.textContent = "You tie! Rock on Rock.";
+                score.textContent = `Computer: ${computerScore} | Human: ${humanScore}`
+
+                // console.log("You tie! Rock on Rock.");
                 break;
-            case 'scissors':
-                console.log("You win! Rock beats scissors.");
+            case 'scissors'://human wins
+                humanScore++;
+                result.textContent = "You win! Rock beats scissors.";
+                score.textContent = `Computer: ${computerScore} | Human: ${humanScore}`
+
+                // console.log("You win! Rock beats scissors.");
                 break;
             default:
-                console.log("What happened?")
+                result.textContent = "OOPS"
+                score.textContent = `Computer: ${computerScore} | Human: ${humanScore}`
+
+                // console.log("What happened?")
         }
     } else {
         switch(computerChoice.toLowerCase()){
-            case 'paper':
-                console.log("You win! Scissors beats paper.");
+            case 'paper'://human wins
+                humanScore++;
+                result.textContent = "You win! Scissors beats paper.";
+                score.textContent = `Computer: ${computerScore} | Human: ${humanScore}`
+
+                // console.log("You win! Scissors beats paper.");
                 break;
-            case 'rock':
-                console.log("You lose! Rock beats scissors.");
+            case 'rock'://computer wins
+                computerScore++;
+                result.textContent = "You lose! Rock beats scissors.";
+                score.textContent = `Computer: ${computerScore} | Human: ${humanScore}`
+
+                // console.log("You lose! Rock beats scissors.");
                 break;
-            case 'scissors':
-                console.log("You tie! Scissors on scissors.");
+            case 'scissors'://tie
+                result.textContent = "You tie! Scissors on scissors.";
+                score.textContent = `Computer: ${computerScore} | Human: ${humanScore}`
+
+                // console.log("You tie! Scissors on scissors.");
                 break;
             default:
-                console.log("What happened?")
+                result.textContent = "OOPS"
+                score.textContent = `Computer: ${computerScore} | Human: ${humanScore}`
+
+                // console.log("What happened?")
         }
     }
     
 }
 
 let computer = getComputerChoice();
-let human = getHumanChoice();
-console.log(`Computer move: ${computer} | Your move: ${human}.`);
-
-playRound(human, computer);
